f531fec6c7a0823f47a67342b7d3e718e72c7af1
虞先達 (Eric S. Yu) <ffk0716@gmail.com>
2021-01-01 13:21:57 +0800

    Update class pipe

--- main.cxx
+++ main.cxx
@@ -1,14 +1,56 @@
 #include <cstdint>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
+class stage {
+  public:
+    vector<uint32_t> in;
+    vector<uint32_t> out;
+    uint32_t time;
+    stage() { time = 1; }
+};
+
+class inst {
+  public:
+    vector<stage> stages;
+    inst() { stages.resize(5); }
+};
 class pipe {
 
   public:
     pipe() {
         regs.resize(4);
         pipes.resize(5);
+    }
+
+    void print() const {
+        cout << "regs:";
+        for (auto &r : regs)
+            cout << " " << r;
+        cout << endl;
+        //
+        cout << "pipes:";
+        for (auto &p : pipes)
+            cout << " " << p;
+        cout << endl;
+    }
+
+    void run(const inst &inst_in) {
+        for (size_t i = 0; i < 5; i++) {
+            auto &s = inst_in.stages.at(i);
+            uint32_t start = pipes.at(i);
+            if (i > 0)
+                start = max(start, pipes.at(i - 1));
+            for (uint32_t r : s.in)
+                start = max(start, regs.at(r));
+            uint32_t end = start + s.time;
+            for (uint32_t r : s.out)
+                regs.at(r) = end;
+            pipes.at(i) = end;
+        }
+        print();
     }
 
   private:
@@ -18,5 +60,19 @@ class pipe {
 
 int main() {
     pipe p;
+    {
+        inst i;
+        p.run(i);
+    }
+    {
+        inst i;
+        i.stages.at(2).out.push_back(3);
+        p.run(i);
+    }
+    {
+        inst i;
+        i.stages.at(1).in.push_back(3);
+        p.run(i);
+    }
     return 0;
 }
